bbb2bc98046ec4ff7871926ac8c06aa0d10a36c9
Adam Cigánek <adam.ciganek@gmail.com>
2010-11-01 13:47:11 +0100

    Adds extensions/Xrender binding

--- examples/simple.d
+++ examples/simple.d
@@ -1,12 +1,12 @@
 // This an example for the X11 binding.
 //
 // Build/run with rdmd:
-//   rdmd -I.. simple.d 
+//   rdmd -I.. simple.d
 
 
 module simple;
 
-import X11.Xlib;
+import x11.xlib;
 import std.c.stdio;
 import std.string;
 
@@ -18,22 +18,21 @@ int main(char[][] args)
         DefaultRootWindow(display),        		// parent window
         0, 0, 200, 100,                    		// x, y, w, h
         0,0x0,0x000000FF                   		// border_width,boder_color,back_color
-        );                                       
+        );
     XMapWindow(display, window); //map the window
     XRaiseWindow(display, window); //show the window
-    XStoreName(display,window, cast(char*) toStringz("Hello Window")); //set window name , don't forget /0 term char !
+    XStoreName(display,window, "Hello Window"); //set window name
     XFlush(display); // flush X server
-    
+
     //wait for a enter pressed (in fact you need to wait for MapNotify event before drawing...)
-    printf("press enter to show window content\0"); 
+    printf("press enter to show window content\0");
     getchar();
-    
+
     XGCValues values;
     values.foreground=0xFFFFFF;
     values.background=0x00FF00;
-    GC gc=XCreateGC(display,window,GCMask.GCForeground | GCMask.GCBackground,&values); //create zone for drawing
-    char* chaine = cast(char*) toStringz("hello world");
-    XDrawString(display, window,gc, 30,50,chaine, 11); //draw string
+    GC gc=XCreateGC(display,window, GCForeground | GCBackground, &values); //create zone for drawing
+    XDrawString(display, window,gc, 30,50, "hello world", 11); //draw string
     XDrawRectangle(display,window,gc,20,20,150,50); //draw rectangle
     XFlush(display); //flush X server
     printf("press enter to close program\0");
